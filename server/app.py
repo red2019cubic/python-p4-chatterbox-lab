e07b6ca543af36415f718217f39607d0d309aed1
Red <red.sarouf2014@gmail.com>
2023-09-10 20:05:13 -0400

    add chatterbox lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -14,13 +15,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+
 @app.route('/messages')
-def messages():
-    return ''
+def fetch_all_messages():
+    messages = Message.query.all()
+    messages_list = [message.to_dict() for message in messages]
+    response = make_response(messages_list, 200)
+    return response
+
+
+@app.route('/messages', methods=['POST',])
+def create_message():
+    msg = request.get_json()
+    new_message = Message(body=msg["body"], username=msg["username"])
+    db.session.add(new_message)
+    db.session.commit()
+    return new_message.to_dict()
+
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    message = Message.query.get(id)
+    response = make_response(message.to_dict(), 200)
+    return response
+
+
+@app.route('/messages/<int:id>', methods=['PATCH',])
+def update_message(id):
+    message = Message.query.get(id)
+    msg = request.get_json()
+    message.body = msg["body"]
+    db.session.add(message)
+    db.session.commit()
+    response = make_response(message.to_dict(), 200)
+    return response
+
+
+@app.route('/messages/<int:id>', methods=['DELETE',])
+def delete_message(id):
+    message = Message.query.get(id)
+    db.session.delete(message)
+    db.session.commit()
+    return "Record Deleted"
+
 
 if __name__ == '__main__':
     app.run(port=5555)
